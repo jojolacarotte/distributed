301dbfedebc35de785665871ca7f18a305f403f9
Joseph <phanjoseph@me.com>
2019-07-19 23:01:24 +0200

    C1

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 LAST_COMMIT=$(git log -1 --pretty=format:'%h %an %s')
 echo "Deploying changes from $LAST_COMMIT"
 gh-pages -r git@github.com:funretro/distributed.git -d dist -m "Updates from $LAST_COMMIT"
+caca
